2e1c42b6928783f4bb50b1b296544e4b91ee6a53
Shayan Firoozi <shayan.firoozi@gmail.com>
2023-12-24 22:37:03 +0330

    Integrate AssemblyInfos Remove Control-Panel useless project.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿/*---------------------------------------------------------------------------------------------
 
-            ► FluentConsole.Net , Working With .Net Console In Elegant Way ! ◄
+                ► FastLog.Net , High Performance Logger For .Net ◄
 
 
 
@@ -8,7 +8,7 @@
 
  → Contact : <shayan.firoozi@gmail.com>
 
- → GitHub repository : https://github.com/ShayanFiroozi/FluentConsole.Net
+ → GitHub repository : https://github.com/ShayanFiroozi/FastLog.Net
 
 ---------------------------------------------------------------------------------------------*/
 
@@ -21,34 +21,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fluent Console For .Net")]
-[assembly: AssemblyDescription("Using .Net Console In Elegant Way !")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TrendSoft® , Bandar Abbas , Iran")]
-[assembly: AssemblyProduct("TrendSoft® FluentConsole.Net")]
-[assembly: AssemblyCopyright("Copyright © 2023 TrendSoft® (MIT License)")]
-[assembly: AssemblyTrademark("Shayan Firoozi")]
-[assembly: AssemblyCulture("")]
-
-
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71B48882-16F9-4779-9C42-E504B2122BD4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("3.7.99.111")]
+[assembly: AssemblyTitle("TrendSoft® Fluent Console for .NET")]
+[assembly: AssemblyDescription("کنسول حرفه‌ای برای دات نت")]
+[assembly: AssemblyProduct("کنسول حرفه‌ای برای دات نت")]
